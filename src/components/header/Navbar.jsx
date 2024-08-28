9f41c240c579193c072def950c66ea5e3cc432fc
Diego Miguel <eduardo.miguelg99@gmail.com>
2024-08-28 11:07:24 -0600

    Estiliza el Navbar con subrayado en el hover en los li

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -5,15 +5,37 @@ export default function Navbar() {
     <>
       <nav className="text-azulin sticky -top-0 flex items-center justify-around bg-rosin py-4 text-lg font-bold">
         <ul className="flex min-w-full justify-around">
-          <a href="/">Inicio</a>
-          <a href="/nosotros">
-            <li>Nosotros</li>
+          <a
+            href="/"
+            className="group text-migomorado transition-all duration-300 ease-in-out"
+          >
+            <li class="bg-gradient-to-r from-migomorado to-migomorado bg-[length:0%_2px] bg-left-bottom bg-no-repeat transition-all duration-500 ease-out group-hover:bg-[length:100%_2px]">
+              Inicio
+            </li>
           </a>
-          <a href="/clientes">
-            <li>Clientes</li>
+          <a
+            href="/nosotros"
+            className="group text-migomorado transition-all duration-300 ease-in-out"
+          >
+            <li class="bg-gradient-to-r from-migomorado to-migomorado bg-[length:0%_2px] bg-left-bottom bg-no-repeat transition-all duration-500 ease-out group-hover:bg-[length:100%_2px]">
+              Nosotros
+            </li>
           </a>
-          <a href="/servicios">
-            <li>Servicios</li>
+          <a
+            href="/clientes"
+            className="group text-migomorado transition-all duration-300 ease-in-out"
+          >
+            <li class="bg-gradient-to-r from-migomorado to-migomorado bg-[length:0%_2px] bg-left-bottom bg-no-repeat transition-all duration-500 ease-out group-hover:bg-[length:100%_2px]">
+              Clientes
+            </li>
+          </a>
+          <a
+            href="/servicios"
+            className="group text-migomorado transition-all duration-300 ease-in-out"
+          >
+            <li class="bg-gradient-to-r from-migomorado to-migomorado bg-[length:0%_2px] bg-left-bottom bg-no-repeat transition-all duration-500 ease-out group-hover:bg-[length:100%_2px]">
+              Servicios
+            </li>
           </a>
         </ul>
       </nav>
