04fdce23bc7c4e6e3200ef12f5e87d98e04f0b0e
Diego Miguel <eduardo.miguelg99@gmail.com>
2024-08-28 16:06:30 -0600

    arregla el about y la seccion de clientes para ser mas estetica y los vuelve responsive

--- src/pages/3.proyectos/Clientes.jsx
+++ src/pages/3.proyectos/Clientes.jsx
@@ -6,80 +6,103 @@ export default function Clientes() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen">
-        <section className="bg-azulin/90 flex min-h-screen flex-row px-5 py-10 text-rosin">
-          <div className="w-1/2">
-            <h2 className="text-amarillin font-extrabold">
-              Somos una agencia internacional
-            </h2>
-            <h3>
+      <div className="">
+        <h2 className="bg-bgwhite pt-10 text-center text-4xl font-extrabold text-migomorado md:px-10 md:py-10 md:text-left md:text-5xl">
+          Somos una agencia internacional
+        </h2>
+        <section className="flex flex-col bg-bgwhite pb-10 text-rosin md:flex-row md:space-x-14 md:px-10">
+          <div className="mx-auto my-10 w-4/5 space-y-10 text-lg md:font-semibold">
+            <p>
               A lo largo de estos años, hemos trabajado con empresas y marcas
               nacionales e internacionales, con proyectos en diversos idiomas
               como el inglés, portugués, francés, alemán, italiano, japonés y
               coreano.
-            </h3>
-            <ul>
-              <p>Actualmente trabajamos con clientes de varios países:</p>
-              <li>México</li>
-              <li>USA</li>
-              <li>Panama</li>
-              <li>Colombia</li>
-              <li>Portugal</li>
-              <li>Argentina</li>
-            </ul>
+            </p>
+            <p>Actualmente trabajamos con clientes de varios países:</p>
+            <div className="m-5 grid grid-cols-3 gap-5">
+              <div>
+                <p className="font-semibold text-migomorado">México</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">Colombia</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">Panama</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">Portugal</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">Argentina</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">USA</p>
+              </div>
+            </div>
           </div>
           <img
-            src={phone}
+            src={marketing}
             alt=""
-            className="min-h-full w-1/2 rounded-3xl object-cover"
+            className="m-auto min-h-full w-4/5 rounded-3xl object-cover md:w-1/2"
           />
         </section>
       </div>
 
-      <section className="bg-azulin/80 flex min-h-screen flex-row py-10 text-rosin">
-        {/*<img src={marketing} alt="" className="object-cover w-1/2 min-h-full" />*/}
-        <div>
-          <h2>Clientes que confían en Migo Digital</h2>
-          <p>
-            Migo Digital es la agencia de Marketing Digital ideal para empresas
-            líderes en su area. Ya sean nacionales o internacionales
-          </p>
-          <div className="m-5 grid grid-cols-3 gap-5">
-            <div>
-              <p className="font-semibold text-black">Nuestro Bosque</p>
-              <p>Kinder</p>
-            </div>
-            <div>
-              <p className="font-semibold text-black">Joca</p>
-              <p>Psicologo</p>
-            </div>
-            <div>
-              <p className="font-semibold text-black">Refricool Costaverde</p>
-              <p>Aires Acondicionados</p>
-            </div>
-            <div>
-              <p className="font-semibold text-black">Nverts</p>
-              <p>Consultoria Financiera</p>
-            </div>
-            <div>
-              <p className="font-semibold text-black">Montse Oscos</p>
-              <p>Psicologa</p>
-            </div>
-            <div>
-              <p className="font-semibold text-black">Unity</p>
-              <p>Corredoras</p>
-            </div>
-            <div>
-              <p className="font-semibold text-black">Royal Prestige</p>
-              <p>Articulos de Cocina</p>
-            </div>
-            <div>
-              <p className="font-semibold text-black">Mc Cofee</p>
-              <p>Parafernalia Canabica</p>
+      <div className="">
+        <h2 className="bg-bgwhite px-5 pt-20 text-center text-4xl font-extrabold text-migomorado md:px-10 md:text-left md:text-5xl">
+          Clientes que confían en nosotros
+        </h2>
+        <section className="flex flex-col bg-bgwhite py-10 text-rosin md:flex-row md:space-x-14 md:px-10">
+          <img
+            src={phone}
+            alt=""
+            className="m-auto min-h-full w-4/5 rounded-3xl object-cover md:w-1/2"
+          />
+
+          <div className="mx-auto my-10 w-4/5 space-y-10 text-lg md:font-semibold">
+            <p>
+              Somos la agencia de Marketing Digital ideal para empresas líderes
+              en su area. Ya sean nacionales o internacionales
+            </p>
+            <div className="m-auto grid grid-cols-3 gap-y-16 md:gap-x-10">
+              <div>
+                <p className="font-semibold text-migomorado">Nuestro Bosque</p>
+                <p>Kinder</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">Joca</p>
+                <p>Psicologo</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">
+                  Refricool Costaverde
+                </p>
+                <p>Aires Acondicionados</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">Nverts</p>
+                <p>Consultoria Financiera</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">Montse Oscos</p>
+                <p>Psicologa</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">Unity</p>
+                <p>Corredoras</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">Royal Prestige</p>
+                <p>Articulos de Cocina</p>
+              </div>
+              <div>
+                <p className="font-semibold text-migomorado">Mc Cofee</p>
+                <p>Parafernalia Canabica</p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </>
   );
 }
--- src/pages/4.nosotros/Nosotros.jsx
+++ src/pages/4.nosotros/Nosotros.jsx
@@ -5,48 +5,57 @@ export default function Nosotros() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen">
-        <section className="flex min-h-screen flex-row bg-bgwhite px-5 py-10 text-rosin">
+      <div className="">
+        <h2 className="bg-bgwhite pt-10 text-center text-4xl font-extrabold text-migomorado md:px-10 md:text-left md:text-5xl">
+          Migo Digital
+        </h2>
+        <section className="flex flex-col bg-bgwhite py-10 text-rosin md:flex-row md:space-x-14 md:px-10">
           <img
             src={marketing}
             alt=""
-            className="min-h-full w-1/2 rounded-3xl object-cover"
+            className="m-auto min-h-full w-4/5 rounded-3xl object-cover md:w-1/2"
           />
 
-          <div className="m-10 w-1/2 space-y-10">
-            <h1 className="text-amarillin font-black">Migo Digital</h1>
-            <h2 className="font-bold">
-              Tus redes sociales con<span className="text-amarillin">MIGO</span>
-            </h2>
+          <div className="mx-auto my-10 w-4/5 space-y-10 text-lg md:font-semibold">
+            <h3 className="font-bold">
+              Tus redes sociales con{" "}
+              <span className="text-migomorado">MIGO</span>
+            </h3>
             <p>
-              Somos la agencia mas eficiente de marketing digital en México y
-              desde la CDMX ofrecemos servicios integrales para marcas y
-              empresas en todo el mundo.
+              Somos la agencia mas eficiente de marketing digital en{" "}
+              <span className="text-migomorado">México</span> y desde la{" "}
+              <span className="text-migomorado">CDMX</span> ofrecemos servicios
+              integrales para marcas y empresas en todo el{" "}
+              <span className="text-migomorado">mundo</span>.
             </p>
-            <p>Hemos desarrollado más de 25 proyectos personalizados</p>
+            <p>
+              Hemos desarrollado más de{" "}
+              <span className="text-migomorado">25 proyectos</span>{" "}
+              personalizados
+            </p>
             <div className="m-5 grid grid-cols-3 gap-5">
               <div>
-                <p className="font-semibold text-black">4</p>
+                <p className="font-semibold text-migomorado">4</p>
                 <p>Años</p>
               </div>
               <div>
-                <p className="font-semibold text-black">4</p>
+                <p className="font-semibold text-migomorado">4</p>
                 <p>Paises</p>
               </div>
               <div>
-                <p className="font-semibold text-black">2</p>
+                <p className="font-semibold text-migomorado">2</p>
                 <p>Continentes</p>
               </div>
               <div>
-                <p className="font-semibold text-black">25+</p>
+                <p className="font-semibold text-migomorado">25+</p>
                 <p>Clientes</p>
               </div>
               <div>
-                <p className="font-semibold text-black">25+</p>
+                <p className="font-semibold text-migomorado">25+</p>
                 <p>Proyectos</p>
               </div>
               <div>
-                <p className="font-semibold text-black">200 hrs +</p>
+                <p className="font-semibold text-migomorado">200 hrs +</p>
                 <p>Contenido Audiovisual</p>
               </div>
             </div>
