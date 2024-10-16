4de1c461f4763d75acd862aa5d4736a6056302c0
Diego Miguel <eduardo.miguelg99@gmail.com>
2024-10-16 13:35:12 -0600

    actualiza texto en Nosotros

--- src/pages/4.nosotros/Nosotros.jsx
+++ src/pages/4.nosotros/Nosotros.jsx
@@ -7,7 +7,7 @@ export default function Nosotros() {
       <Navbar />
       <div className="">
         <h2 className="bg-bgwhite pt-10 text-center text-4xl font-extrabold text-migomorado md:px-10 md:text-left md:text-5xl">
-          Migo Digital
+          Nosotros
         </h2>
         <section className="flex flex-col bg-bgwhite py-10 text-rosin md:flex-row md:space-x-14 md:px-10">
           <img
@@ -17,27 +17,22 @@ export default function Nosotros() {
           />
 
           <div className="mx-auto my-10 w-4/5 space-y-10 text-lg md:font-semibold">
-            <h3 className="font-bold">
-              Tus redes sociales con{" "}
-              <span className="text-migomorado">MIGO</span>
-            </h3>
             <p>
-              Somos la agencia mas eficiente de{" "}
-              <span className="text-migomorado">Diseño</span> con{" "}
-              <span className="text-migomorado">Marketing</span> digital en{" "}
-              <span className="text-migomorado">México</span> y desde la{" "}
-              <span className="text-migomorado">CDMX</span> ofrecemos servicios
-              integrales para marcas y empresas en todo el{" "}
-              <span className="text-migomorado">mundo</span>.
+              Somos una agencia especializada en{" "}
+              <span className="text-migomorado">Comunicación Digital</span> en{" "}
+              <span className="text-migomorado">México</span>. Desde la{" "}
+              <span className="text-migomorado">Ciudad de México</span>{" "}
+              ofrecemos servicios integrales para marcas y empresas de{" "}
+              <span className="text-migomorado">Todo el Mundo</span>.
             </p>
             <p>
               Hemos desarrollado más de{" "}
-              <span className="text-migomorado">25 proyectos</span>{" "}
-              personalizados
+              <span className="text-migomorado">1000 Diseños</span>{" "}
+              personalizados.
             </p>
             <div className="m-5 grid grid-cols-3 gap-5">
               <div>
-                <p className="font-semibold text-migomorado">4</p>
+                <p className="font-semibold text-migomorado">3</p>
                 <p>Años</p>
               </div>
               <div>
@@ -49,15 +44,15 @@ export default function Nosotros() {
                 <p>Continentes</p>
               </div>
               <div>
-                <p className="font-semibold text-migomorado">25+</p>
+                <p className="font-semibold text-migomorado">10+</p>
                 <p>Clientes</p>
               </div>
               <div>
-                <p className="font-semibold text-migomorado">25+</p>
+                <p className="font-semibold text-migomorado">10+</p>
                 <p>Proyectos</p>
               </div>
               <div>
-                <p className="font-semibold text-migomorado">200 hrs +</p>
+                <p className="font-semibold text-migomorado">200 hrs+</p>
                 <p>Contenido Audiovisual</p>
               </div>
             </div>
