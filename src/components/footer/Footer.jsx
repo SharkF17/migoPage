69782a1e3b32e63b476176ba0102caf2dc6ce7c5
Diego Miguel <eduardo.miguelg99@gmail.com>
2024-10-16 20:50:55 -0600

    simplifica footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,11 +5,11 @@ export default function Footer() {
   return (
     <>
       <div className="relative bottom-0 flex w-full flex-col items-center justify-center space-x-5 bg-migomorado text-white">
-        <div className="flex w-full flex-row justify-around">
+        {/*<div className="flex w-full flex-row justify-around">
           <a href="/blog">Blog</a>
           <a href="/espacios">Espacios</a>
         </div>
-        {/* Estilizar link para blog */}
+        // Estilizar link para blog */}
 
         <p className="flex flex-row">
           Hecho con ❤️ desde México. Migo Digital 2024.
