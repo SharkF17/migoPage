10846d75304002eef3ceca1fd1b26aafebf4009a
Diego Miguel <eduardo.miguelg99@gmail.com>
2024-11-26 10:12:22 -0600

    cambios esteticos en la seccion Equipo mejorando el Responsive Design

--- src/pages/7.equipo/Equipo.jsx
+++ src/pages/7.equipo/Equipo.jsx
@@ -17,8 +17,8 @@ export default function Equipo() {
         <div className="flex flex-col bg-bgwhite pt-10 text-center text-4xl font-extrabold text-migomorado md:px-10 md:py-10 md:text-left md:text-5xl">
           <h2>Equipo</h2>
 
-          <div className="my-10 w-4/5 space-y-10 text-lg font-semibold">
-            <ul className="mt-14 space-y-10">
+          <div className="mb-10 w-4/5 space-y-10 text-lg font-semibold md:mt-10">
+            <ul className="mt-14 space-y-10 pl-5 text-left">
               {EQUIPO.map(({ nombre, puesto }) => (
                 <li>
                   {nombre + " - "}
@@ -32,7 +32,7 @@ export default function Equipo() {
           <img
             src={marketing}
             alt=""
-            className="m-auto w-5/6 rounded-3xl object-cover pb-5 md:w-4/5"
+            className="m-auto w-4/5 rounded-3xl object-cover pb-5 md:w-4/5"
           />
         </div>
       </div>
